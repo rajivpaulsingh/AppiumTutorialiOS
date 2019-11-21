c0ea051d95d52a221ce9c3063b9c5c6765ab642c
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-11-20 16:41:32 -0800

    Converted base class to capabilities + added UIKitCatalog app

--- src/Base.java
+++ src/Base.java
@@ -1,4 +1,5 @@
 import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,7 +9,7 @@ import java.net.URL;
 
 public class Base {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static IOSDriver<IOSElement> Capabilities() throws MalformedURLException {
 
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
@@ -17,6 +18,7 @@ public class Base {
         desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/singh2_rajiv/Library/Developer/Xcode/DerivedData/UIKitCatalog-caymnoohisuimeburqrhimsticrm/Build/Products/Debug-iphonesimulator/UIKitCatalog.app");
 
         IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        return driver;
 
     }
 }
